338f8eeb9e5b4ae0336c5bf5d9319343f5b0ba33
Abdelrahman Ahmed <abdelrahman.ahmed.mail@gmail.com>
2023-09-08 05:46:16 +0300

    test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">Monsters Cool Rolodex</h1>
       <SearchBox
         className="search-box"
         placeholder="search monsters"
